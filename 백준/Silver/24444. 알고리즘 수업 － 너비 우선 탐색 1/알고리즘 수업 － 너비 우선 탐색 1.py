5695389d79f02b0c3222d48c7e75f6686afa512c
wjstp <124495992+wjstp@users.noreply.github.com>
2023-08-19 00:15:05 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 620 ms, Memory: 63696 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+N, M, R = map(int, input().split())
+adj_list = [[] for _ in range(N+1)]
+for _ in range(M):
+    u, v = map(int, input().split())
+    adj_list[u].append(v)
+    adj_list[v].append(u)
+
+for i in range(N+1) :
+    adj_list[i].sort()
+
+num = 1
+num_arr = ['0']*(N+1)
+bfsq = deque()
+visited = [0]*(N+1)
+bfsq.append(R)
+visited[R] = 1
+while bfsq :
+    cnode = bfsq.popleft()
+    num_arr[cnode] = str(num)
+    num += 1
+    for a in adj_list[cnode] :
+        if visited[a] != 1:
+            bfsq.append(a)
+            visited[a] = 1
+print('\n'.join(num_arr[1:]))
