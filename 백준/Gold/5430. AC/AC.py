ee73619bcbeb00d495d18ee357a3b724e5972979
wjstp <124495992+wjstp@users.noreply.github.com>
2023-11-13 21:12:27 +0900

    [Gold V] Title: AC, Time: 204 ms, Memory: 42620 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -6,43 +6,30 @@ T = int(input().rstrip())
 for _ in range(T) :
     cmd = input().rstrip()
     n = int(input().rstrip())
-    arr_str = input().rstrip()
-    # 배열로 변환
-    arr = deque()
-    tmp = ''
-    flag = False
-    for ele in arr_str:
-        if ele.isdigit() :
-            tmp += ele
-            flag = True
-        else :
-            if flag :
-                arr.append(tmp)
-            tmp = ''
-    flag = True
-    exitFlag = False
+    arr = deque(input().rstrip()[1:-1].split(','))
+    if n == 0 :
+        arr = deque()
+    is_reversed = False
+    not_error = True
     cnt = 0
     for c in cmd :
-        # print(arr)
         if c == 'D' :
-            if len(arr) :
-                if flag :
-                    arr.popleft()
-                else :
-                    arr.pop()      
-            else :
-                arr = 'error'
-                exitFlag = True
+            if not len(arr) :
+                print('error')
+                not_error = False
                 break
+            if is_reversed :
+                arr.pop()      
+            else :
+                arr.popleft()
         elif c == 'R':
-            flag = not flag
+            is_reversed = not is_reversed
             cnt += 1
         
-    if exitFlag :
-        print(arr)
-    else :
-        if cnt %2 : # 뒤집히는 경우
-            ans = '[' + ','.join(reversed(arr)) +']'
+    if not_error :
+        if is_reversed : # 뒤집히는 경우
+            print('[' + ','.join(reversed(arr)) +']')
         else :
-            ans = '[' + ','.join(arr) +']'
-        print(ans)
+            print('[' + ','.join(arr) +']')
+        
+
