dcb3cd3e5253afefaadae87ab1e250a0d3d7f4f6
wjstp <124495992+wjstp@users.noreply.github.com>
2024-01-13 18:20:13 +0900

    [level unrated] Title: 산 모양 타일링, Time: 46.54 ms, Memory: 14.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258705. 산 모양 타일링/산 모양 타일링.py
@@ -0,0 +1,18 @@
+def solution(n, tops):
+    answer = 0
+    dp = [0 for _ in range(2*n+2)] # dp[i] i번째까지 모양을 만들 수 있는 경우의 수
+    dp[0] = 1 # 아무것도 없을 떄
+    dp[1] = 1
+    for i in range(2, 2*n+2):
+        if i % 2 == 0: # 짝수
+            if tops[i//2-1] : # 뿔이 있는 경우
+                dp[i] = (dp[i-1] * 2 + dp[i-2]) % 10007
+            else :
+                dp[i] = (dp[i-1] + dp[i-2]) % 10007
+        else : # 홀수
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007
+            
+                
+    # print(dp)
+    answer = dp[2*n+1]
+    return answer
