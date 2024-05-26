b2b6ea1a0b9b413478f6e247f60363aa23563cc3
wjstp <124495992+wjstp@users.noreply.github.com>
2024-05-26 18:00:33 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,5 +1,5 @@
 -- 스킬코드가 일치하는 개발자의 id, 이메일, 이름, 성 조회, id기준 오름차순
--- 비트연산 활용 100100(a)에 100(b)이 있는지 확인? a & b = b
+-- 비트연산 활용 개발자 코드 & 프론트엔드 코드 겹치는게 있다면 0은 아니다. 
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
 FROM DEVELOPERS 
 WHERE SKILL_CODE & (SELECT sum(CODE) 
