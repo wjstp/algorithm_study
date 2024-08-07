f2af8de4f50939f1d8d6e7d3934848c4cf2eb19c
wjstp <124495992+wjstp@users.noreply.github.com>
2024-08-08 02:28:24 +0900

    [Gold IV] Title: 가르침, Time: 284 ms, Memory: 15372 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,79 +1,58 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int k;
-    static boolean[] visited;
-    static String[] word;
-    static int answer = 0;
+    static int N, K, answer;
+    static String[] words;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stz = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(stz.nextToken()); // 주어진 단어 수
+        K = Integer.parseInt(stz.nextToken()); // 배울 수 있는 글자 수
 
-        String[] input = br.readLine().split(" ");
-
-        n = Integer.parseInt(input[0]);
-        k = Integer.parseInt(input[1]);
-
-        visited = new boolean[26];
-        word = new String[n];
-
-        if (k < 5) {
-            System.out.println(0);
-            return;
-        } else if (k == 26) {
-            System.out.println(n);
-            return;
+        if(K < 5 || K == 26) {
+            answer = K < 5? 0 : N;  // 배울 수 있는 숫자가 크면 굳이 필요 없다.
+        } else {
+            words = new String[N];
+            for (int i = 0; i < N; i ++)
+                words[i] = br.readLine().replaceAll("anta|tica", "");
+            int visit = 0;
+            visit += 1 << ('a' - 'a');
+            visit += 1 << ('n' - 'a');
+            visit += 1 << ('t' - 'a');
+            visit += 1 << ('c' - 'a');
+            visit += 1 << ('i' - 'a');
+            dfs(0, 0, visit);
         }
 
-        /* 무조건 배워야하는 단어 */
-        visited['a' - 'a'] = true;
-        visited['n' - 'a'] = true;
-        visited['t' - 'a'] = true;
-        visited['i' - 'a'] = true;
-        visited['c' - 'a'] = true;
-
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine().replaceAll("anta|tica", "");
-
-            word[i] = str;
-        }
-
-        check(0, 0);
         System.out.println(answer);
     }
 
-    static void check(int alpha, int count) {
-        if (count == k - 5) {
-            int temp = 0;
-            for (int i = 0; i < n; i++) {
-                boolean flag = true;
-
-                for (int j = 0; j < word[i].length(); j++) {
-                    /* 배우지않은 알파벳이 있는 경우 */
-                    if (!visited[word[i].charAt(j) - 'a']) {
-                        flag = false;
+    public static void dfs(int idx, int cnt, int visit) {// idx, 읽을 줄 아는 글자수
+        if (cnt == K - 5) {
+            int tmp = 0;
+            for (int i = 0; i < N; i ++) {
+                boolean readable = true;
+                for (int j = 0; j < words[i].length() ; j ++) {
+                    if ((visit & (1 << (words[i].charAt(j) - 'a'))) == 0) {
+                        readable = false;
                         break;
                     }
                 }
+                if (readable) tmp ++ ;
 
-                if (flag) {
-                    temp++;
-                }
             }
-            answer = Math.max(temp, answer);
+            answer = Math.max(tmp, answer);
             return;
         }
-
-        for (int i = alpha; i < 26; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                check(i, count + 1);
-                visited[i] = false;
-            }
+        for (int i = idx; i < 26; i ++) {
+            if ((visit & (1 << i)) == 0)
+                dfs(i + 1, cnt + 1, visit | 1 << i);
         }
     }
+
+
 
 }
