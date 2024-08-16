748d47b173fe8240e1c968e252fe371a040865e3
wjstp <124495992+wjstp@users.noreply.github.com>
2024-08-16 16:47:01 +0900

    [Gold IV] Title: 단어 수학, Time: 848 ms, Memory: 16576 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,56 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
+// 시간 복잡도 나올 수 있는 알파벳 수 최대 10.. 거기에 들어갈 수 있는 숫자 조합 10!..대충 300만
+// 여기에 숫자들 합 구하면서 10 * 10 -> 3억
+// 시간복잡도 확인해볼 것
+// 이거 Map으로 풀어도 시간초과남. 다음 번엔 그리디로 최적화할 것
 public class Main {
-    static int N, alphabet, answer;
+    static int N, letter, answer;
     static String[] words;
-
-    static int[] wordMath;
-    static List<Integer> letters;
-    static int visit;
+    static int[] letterMap; // 단어에서 글자가 어떤 수를 가지는지 바로 접근할 용도
+    static List<Integer> letters;  // 어떤 키들이 있는지 기록하는 용도. dfs에서 키를 순서대롲 점근 어떤 글자들이 있는지 인덱스에서 맵으로 접근할 용도
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         words = new String[N];
-        wordMath = new int[26];    // a~z
+        letterMap = new int[26];
         letters = new ArrayList<>();
         for (int i = 0; i < N; i ++) {
             words[i] = br.readLine();
-            for (int j = 0; j < words[i].length() ; j ++) {
-                if (letters.contains(words[i].charAt(j) - 'A')) continue;
-                letters.add(words[i].charAt(j) - 'A');
-                alphabet ++ ;
+            for (int j = 0; j < words[i].length(); j ++) {
+                if (letterMap[words[i].charAt(j) - 'A'] == 0) {
+                    letterMap[words[i].charAt(j) - 'A'] = 1;
+                    letter ++ ;
+                    letters.add(words[i].charAt(j) - 'A');
+                }
             }
         }
-        dfs(0);
+        dfs(0, 0);
         System.out.println(answer);
     }
-    public static void dfs(int cur) {
-        if (cur == alphabet) {
+
+    public static void dfs(int letterIdx, int check) {
+        if (letterIdx == letter) {
             answer = Math.max(answer, getSum());
-            return;
         }
-        for (int i = 9 ; i >= 10 - alphabet ; i--) {    // 10개 다 돌 필요없음
-            if ((visit & (1 << i)) == 0) {
-                wordMath[letters.get(cur)] = i;
-                visit |= 1 << i;
-                dfs(cur + 1);
-                visit &= ~(1 << i);
-            }
+        for (int i = 9; i > 9 - letter; i --) {
+            if ((check & 1 << i) == 1 << i) continue;
+            check |= 1 << i;
+            letterMap[letters.get(letterIdx)] = i;
+            dfs(letterIdx + 1, check);
+            check &= ~(1 << i);
         }
     }
-
     public static int getSum() {
         int sum = 0;
         for (int i = 0; i < N; i ++) {
-            int len = words[i].length();
-            int pow = 1;
-            for (int j = len - 1; j >= 0; j --) {
-                sum += wordMath[words[i].charAt(j) - 'A'] * pow;
-                pow *= 10;
+            int ten = 1;
+            for (int j = words[i].length() - 1; j >= 0 ; j --) {
+                sum += letterMap[words[i].charAt(j) - 'A'] * ten;
+                ten *= 10;
             }
         }
         return sum;
