e898784a3ba099d996256fa8f2865292d7119621
wjstp <124495992+wjstp@users.noreply.github.com>
2024-10-01 20:29:04 +0900

    [Gold II] Title: 철로, Time: 696 ms, Memory: 47592 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.java
+++ 백준/Gold/13334. 철로/철로.java
@@ -1,64 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+import java.util.Comparator;
 
-// 입력받을 떄 집과 사무실의 위치가 뒤집혀서 마이너스가 되는 경우는 고려안함
-// peek으로만 접근해서 ㅃ바져야 할 값이 빠지지 않아 그 다음 값으로 넘어가지 못하고 계쏙 조건이 충족이 안돼 poll이 안됐었음
-// 정렬한 것을 뛰어넘어서 범위에 포함되는 경우 처리를 하지 않았음
-// 답봤다.. 끝지점을 기준으로 오름차순, 끝지점이 같다면 시작지점이 앞에 있는 것 (길이가 긴 순으로)
-// 가장 앞에 있는 것이 그 다음것보다 무조건 범위에서 먼저 빠져나가는 것일 수 있도록
 public class Main {
-    static int N, d, answer;
-    static PriorityQueue<Point> people;
-    static PriorityQueue<Point> currentRailWay;
-
-    static class Point implements Comparable<Point> {
-        int start;
-        int end;
-
-        Point(int start, int end) {
-            this.start = start;
-            this.end = end;
+    static int n, d, cnt, answer;
+    static Info[] infos;
+    static PriorityQueue<Info> pq;
+    static class Info implements Comparable<Info>{
+        int s;
+        int e;
+        Info(int s, int e) {
+            this.s = s;
+            this.e = e;
         }
 
         @Override
-        public int compareTo(Point point) {
-            if (this.end == point.end) {
-                return this.start - point.start;
-            } else {
-                return this.end - point.end;
-            }
+        public int compareTo(Info o) {
+            if (this.e == o.e) return this.s - o.s;
+            return this.e - o.e; // 시작점 기준 오름차순
         }
     }
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        people = new PriorityQueue<>();
-        for (int i = 0; i < N; i++) {
-            StringTokenizer stz = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer stz;
+        infos = new Info[n];
+        for (int i = 0; i < n; i++) {
+            stz = new StringTokenizer(br.readLine());
             int h = Integer.parseInt(stz.nextToken());
             int o = Integer.parseInt(stz.nextToken());
-            people.add(new Point(Math.min(h, o), Math.max(h, o)));
+            infos[i] = new Info(Math.min(h, o), Math.max(h, o));
         }
+        Arrays.sort(infos);
         d = Integer.parseInt(br.readLine());
-        currentRailWay = new PriorityQueue<>(Comparator.comparingInt(p -> p.start));
-        answer = 0;
-        int cnt = 0;
 
-        while (!people.isEmpty()) {
-            Point current = people.poll();
-            currentRailWay.add(current);
-            cnt++;
+        pq = new PriorityQueue<>(Comparator.comparingInt(p->p.s));
 
-            while (!currentRailWay.isEmpty() && currentRailWay.peek().start < current.end - d) {
-                currentRailWay.poll();
-                cnt--;
+        for (int i = 0; i < n; i ++) {
+            pq.add(infos[i]);
+            cnt ++;
+            while (! pq.isEmpty() && pq.peek().s + d < infos[i].e) {
+                pq.poll();
+                cnt --;
             }
-
             answer = Math.max(answer, cnt);
         }
-
         System.out.println(answer);
     }
+
 }
